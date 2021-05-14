ec1201a0f32aebcd66bbc9d25cb14ac27e819fd0
Dorian Guitel <dorian.guitel@gmail.com>
2021-05-14 12:16:39 +0200

    changed root

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "bookmarks/new"
   get "bookmarks/create"
   get "bookmarks/delete"
+  root to: 'lists#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :lists, only: [:new, :create, :show, :index] do
     resources :bookmarks, only: [:new, :create]
