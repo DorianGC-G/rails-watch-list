e72057bd81b34895f1ee0a7aa132ff35e65aa710
Dorian Guitel <dorian.guitel@gmail.com>
2021-05-14 16:55:57 +0200

    advanced on style

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,6 +23,7 @@ page_number = 1
   end
   page_number += 1
 end
+
 # Movie.create(title: "Trainspotting", overview: "A group of young Scottish heroin addicts prepares a massive drug deal.", poster_url: "https://fr.web.img6.acsta.net/medias/nmedia/18/65/54/67/19816073.jpg", rating: 7.2)
 # Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
 # Movie.create(title: "Clerks", overview: "A day in the lives of two convenience clerks named Dante and Randal as they annoy customers, discuss movies, and play hockey on the store roof.", poster_url: "https://fr.web.img4.acsta.net/medias/nmedia/18/66/06/54/19253723.jpg", rating: 7.7)
